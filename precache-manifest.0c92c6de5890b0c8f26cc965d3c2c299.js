2ab94d8c1f1d7dc6a1525ec725562724b7f9203e
Vincent <vincent.sylvestre@hotmail.com>
2020-03-16 10:08:41 -0400

    Updates

---
+++ precache-manifest.0c92c6de5890b0c8f26cc965d3c2c299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df144b6f3f2ae66b554926dc1bd3666",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4fbe2aef82a44c9c8ee3",
+    "url": "/Monsters-rolodex/static/css/main.79258ee8.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/Monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fbe2aef82a44c9c8ee3",
+    "url": "/Monsters-rolodex/static/js/main.285caf9a.chunk.js"
+  },
+  {
+    "revision": "f50133f2c4b44c266753",
+    "url": "/Monsters-rolodex/static/js/runtime-main.da3dab2d.js"
+  }
+]);
